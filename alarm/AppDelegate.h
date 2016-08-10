d46757d460749bb9cb05dd3499abd41a6075d1c2
bin.shen <bin.shen@openx.com>
2016-08-10 16:08:34 +0800

    Initial Commit

---
+++ alarm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  alarm
+//
+//  Created by Bin Shen on 8/10/16.
+//  Copyright © 2016 Bin Shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
